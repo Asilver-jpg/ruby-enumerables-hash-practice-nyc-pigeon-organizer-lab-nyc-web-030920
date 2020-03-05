187bc88dfe8516aaee09ea002ba1b4b613c80491
Alexander Silver <asilver7493@gmail.com>
2020-03-05 19:20:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list= {}
+ data.each do |qualifier, val|
+  val.each do |quality, value|
+    value.each do |name|
+       
+      if !(pigeon_list.has_key?(name))
+          pigeon_list[name] = {}
+      if !(pigeon_list[name].has_key?(qualifier))
+        pigeon_list[name][qualifier] = [quality]
+      else 
+        pigeon_list[name][qualifier].push(quality)
+      end
+    end
+  end
+ end
+
+end
+
+ pigeon_list
 end
