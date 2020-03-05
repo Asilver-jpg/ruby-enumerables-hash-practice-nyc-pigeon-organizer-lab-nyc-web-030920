f3758d05209c7a1431d2c3e79de442fa3404cea2
Alexander Silver <asilver7493@gmail.com>
2020-03-05 19:53:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list= {}
+ data.each do |qualifier, val|
+  val.each do |quality, value|
+    value.each do |name|
+       
+      if !(pigeon_list.has_key?(name))
+          pigeon_list[name] = {}
+        end
+      if !(pigeon_list[name].has_key?(qualifier))
+        pigeon_list[name][qualifier] = []
+      end
+     pigeon_list[name][qualifier].push(quality.to_s)
+  end
+ end
+
+end
+
+ pigeon_list
 end
